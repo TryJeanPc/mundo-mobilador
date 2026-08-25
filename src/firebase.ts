6557f4d0af267751ee5e81f9194ea1fd20e9ce1d
JeanDavid <104921160+TryJeanPc@users.noreply.github.com>
2026-08-25 00:23:02 -0500

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,15 +3,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDmrp-rv4nmY6jqGcxrY5duxXwRN9tKm8I",
-  authDomain: "sodium-cyclist-klcf1.firebaseapp.com",
-  projectId: "sodium-cyclist-klcf1",
-  storageBucket: "sodium-cyclist-klcf1.firebasestorage.app",
-  messagingSenderId: "23985755032",
-  appId: "1:23985755032:web:ab32cacfb7c0c9f2ee11ed"
+  apiKey: "AIzaSyDYHvOB2RtYGKo_WE6wecNIYepWdf0N1zw",
+  authDomain: "mundo-mobilador.firebaseapp.com",
+  projectId: "mundo-mobilador",
+  storageBucket: "mundo-mobilador.firebasestorage.app",
+  messagingSenderId: "589476597524",
+  appId: "1:589476597524:web:dda4d2b2f55cf759d3fc01",
+  measurementId: "G-XRNN8NP8HW"
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, "ai-studio-mundomobilador-bc82c7ce-6308-4bc1-9820-2f5096a7cede");
+export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
